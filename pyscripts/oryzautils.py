5ed681925919b3bcd7c53fd5485125a6f2a84996
Derrick Zwickl <zwickl@Derricks-MacBook-Pro-3.local>
2011-10-11 13:28:04 -0700

    taxon name update

--- pyscripts/oryzautils.py
+++ pyscripts/oryzautils.py
@@ -4,6 +4,6 @@ import sys
 
 class Oryza:
     def __init__(self):
-        self.taxon_names = [ 'O. sativa AA', 'O. barthii AA', 'O. brachyantha FF', 'O. glaberrima AA', 'O. minuta BB', 'O. minuta CC', 'O. nivara AA', 'O. officinalis CC', 'O. punctata BB', 'O. rufipogon AA' ]
+        self.taxon_names = [ 'O. sativaj AA', 'O. barthii AA', 'O. brachyantha FF', 'O. glaberrima AA', 'O. minuta BB', 'O. minuta CC', 'O. nivara AA', 'O. officinalis CC', 'O. punctata BB', 'O. rufipogon AA' ]
 
 oryza = Oryza()
