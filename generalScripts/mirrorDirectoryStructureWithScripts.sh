1025190010a33b4dea29bef420387111d08157bf
zwickl <zwickl@hal.biosci.arizona.edu>
2012-01-11 10:39:29 -0700

    directory mirror script

---
+++ generalScripts/mirrorDirectoryStructureWithScripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! "$#" -eq 2 ];then
+    echo "enter source and destination directory names"
+    exit
+fi
+
+if [ ! -d "$1" ];then
+    echo $1 is not a directory!
+    exit
+fi
+
+FROM=${1%/}
+TO=${2%/}
+
+rsync -av --include=*/ --include=*.py --include=*.sh --exclude=*  $FROM/ $TO/
