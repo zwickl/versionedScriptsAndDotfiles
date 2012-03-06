1b434d15215a388cc54fba98faec92600218e8b9
Derrick Zwickl <zwickl@Derricks-MacBook-Pro-3.local>
2012-03-06 15:34:05 -0700

    oryza updates

--- pyscripts/oryzautils.py
+++ pyscripts/oryzautils.py
@@ -7,6 +7,13 @@ from dzutils import *
 class Oryza:
     def __init__(self):
         self.taxon_names = [ 'O. sativaj AA', 'O. sativai AA', 'O. barthii AA', 'O. brachyantha FF', 'O. glaberrima AA', 'O. minuta BB', 'O. minuta CC', 'O. nivara AA', 'O. officinalis CC', 'O. punctata BB', 'O. rufipogon AA' ]
+        self.short_names = [ 'OsatjAA', 'OsatiAA', 'ObartAA', 'ObracFF', 'OglabAA', 'OminuBB', 'OminuCC', 'OnivaAA', 'OoffiCC', 'OpuncBB', 'OrufiAA' ]
+        self.short_to_long = dict( [ (self.short_names[n], self.taxon_names[n]) for n in range(0, len(self.taxon_names)) ])
+        self.long_to_short = dict( [ (self.taxon_names[n], self.short_names[n]) for n in range(0, len(self.taxon_names)) ])
+    def short_name_to_long(self, short):
+        return self.short_to_long[short]
+    def long_name_to_short(self, short):
+        return self.long_to_short[short]
 
 oryza = Oryza()
 
