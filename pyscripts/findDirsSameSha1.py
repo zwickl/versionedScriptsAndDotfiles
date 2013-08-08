e036d8d4913b208af3d16cbb8e5f35fdb720ddbf
Derrick Zwickl <zwickl@DizzyLion.local>
2013-08-08 13:26:47 -0700

    script update

--- pyscripts/findDirsSameSha1.py
+++ pyscripts/findDirsSameSha1.py
@@ -3,48 +3,39 @@
 import hashlib, sys, os
 from os.path import normpath, walk, isdir, isfile, dirname, basename, \
     exists as path_exists, join as path_join
-from itertools import chain
 
 def recursive_superdir_generator(path):
     if not path:
         return
     yield path
-    for super in recursive_superdir_generator(os.path.split(path)[0]):
-        yield super
+    for sup in recursive_superdir_generator(os.path.split(path)[0]):
+        yield sup
 
 
 def is_subdir(path, superdir_set):
-    #if isinstance(superdir_list, str):
-    #    superdir_list = [superdir_list]
-    for super in recursive_superdir_generator(path):
-        if super in superdir_set:
-            print "subdir!", super, path
+    if isinstance(superdir_set, list):
+        superdir_set = set(superdir_set)
+    if not superdir_set:
+        return False
+    shortest = min([len(superdir) for superdir in superdir_set])
+    for sup in recursive_superdir_generator(path):
+        if len(sup) < shortest:
+            return False
+        #print path, sup, superdir_set
+        if sup in superdir_set:
+            #print "subdir!", sup, path
             return True
     return False
         
-    exit()
-    
-    for super in superdir_list:
-        if super + '/' in path:
-            return True
-    return False
-
 
 def recursive_subdir_generator(path):
+    contents = os.listdir(path)
+    if not contents:
+        return
     yield path
-    for sub_dir in [ path_join(path, sub_obj) for sub_obj in os.listdir(path) if isdir(path_join(path, sub_obj)) and sub_obj[0] != '.' ]:
+    #for sub_dir in [ path_join(path, sub_obj) for sub_obj in os.listdir(path) if isdir(path_join(path, sub_obj)) and sub_obj[0] != '.' ]:
+    for sub_dir in [ path_join(path, sub_obj) for sub_obj in contents if isdir(path_join(path, sub_obj)) and sub_obj[0] != '.' ]:
         for sub_sub_dir in recursive_subdir_generator(sub_dir):
-            yield sub_sub_dir
-
-
-def filtered_recursive_subdir_generator(path, skips):
-    print path, skips
-    for skip in skips:
-        if skip in path:
-            return
-    yield path
-    for sub_dir in [ path_join(path, sub_obj) for sub_obj in os.listdir(path) if isdir(path_join(path, sub_obj)) and sub_obj[0] != '.' ]:
-        for sub_sub_dir in [ ssd for ssd in filtered_recursive_subdir_generator(sub_dir, skips) if ssd is not None ]:
             yield sub_sub_dir
 
 
@@ -88,29 +79,28 @@ skip_count = 0
 match_count = 0
 for top_dir in sys.argv[1:]:
     if isdir(top_dir):
-        for dir in recursive_subdir_generator(top_dir):
+        for dirname in recursive_subdir_generator(top_dir):
         #for dir in filtered_recursive_subdir_generator(top_dir, ignore_paths):
-            if not is_subdir(dir, ignore_paths):
-                checksum = path_checksum([dir])
+            #print ignore_paths, dirname 
+            dirname = normpath(dirname)
+            if not is_subdir(dirname, ignore_paths):
+                checksum = path_checksum([dirname])
+                #print checksum, dirname
                 count += 1
                 if checksum in hash_dict:
                     #don't consider a match between a directory and it's lone subdirectory
-                    ok = True
-                    for found_dir in hash_dict[checksum]:
-                        if found_dir + '/' in dir:
-                            ok = False
-                            break
-                    if ok:
-                        hash_dict[checksum].append(dir)
+                    #print dirname, set(hash_dict[checksum])
+                    if not is_subdir(dirname, hash_dict[checksum]):
+                        hash_dict[checksum].append(dirname)
                         match_count += 1
                         #this will keep it from recursing any further if a higher level dir matched
-                        ignore_paths.add(dir)
-                        #print hash_dict[checksum]
+                        ignore_paths.add(dirname)
+                        print hash_dict[checksum]
                 else:
-                    hash_dict[checksum] = [dir]
+                    hash_dict[checksum] = [dirname]
             else:
                 skip_count += 1
-            if count % 10 == 0:
+            if count % 50 == 0:
                 print count, skip_count, match_count
 
 #print hash_dict
