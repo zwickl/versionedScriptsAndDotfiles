0d21608319491e1ca6305acecfb438558f5e97a3
Derrick Zwickl <dzwickl@gmail.com>
2013-08-12 16:01:53 -0700

    set script additions

--- generalScripts/setupNewSystem.sh
+++ generalScripts/setupNewSystem.sh
@@ -1,11 +1,61 @@
 #!/bin/bash
 
-sudo apt-get install git vim
+<<TEMP
+###########################
+#General stuff
+###########################
+ssh-keygen
+
+sudo apt-get install --assume-yes git vim
+sudo apt-get install --assume-yes build-essential autoconf subversion libtool
+
+sudo apt-get install --assume-yes matplotlib python-numpy python-scipy r-base 
 
 git clone ssh://zwickl@phylo.bio.ku.edu/~/gitRepos/versionedScriptsAndDotfiles.git
+$HOME/versionedScriptsAndDotfiles/linkFiles.sh
+source $HOME/.bashrc
+TEMP
 
-sudo apt-get install build-essential  subversion matplotlib python-numpy python-scipy r-base 
+<<TEMP
+###########################
+#NCL
+###########################
+mkdir -p $HOME/ncl
+NROOT=$HOME/ncl/ncl-2.1
+svn checkout http://svn.code.sf.net/p/ncl/code/branches/v2.1 $NROOT
+mkdir -p $NROOT/builds/gccNoDynamic
+cd $NROOT
+./bootstrap.sh
+cd builds/gccNoDynamic
+env CXXFLAGS="-DNCL_CONST_FUNCS" ../../configure --prefix=`pwd`/installed --disable-shared
+make && make check && make install
+cd $HOME
+###########################
 
-svn checkout https://garli.googlecode.com/svn/garli/ googleCodeRepo --username dzwickl@gmail.com 
-svn checkout http://svn.code.sf.net/p/ncl/code/branches/v2.1 ncl-2.1
+<<TEMP
+###########################
+#BOINC
+###########################
+git clone git://boinc.berkeley.edu/boinc-v2.git boinc-v2
+sudo apt-get install libcurl4-openssl-dev libfcgi-dev
+cd boinc-v2
+./_autosetup
+./configure
+exit
+TEMP
+
+<<TEMP
+###########################
+#GARLI
+###########################
+svn checkout https://garli.googlecode.com/svn/garli googleCodeRepo --username dzwickl@gmail.com
+TEMP
+
+###########################
+#for beagle
+###########################
+sudo apt-get install nvidia-opencl-dev
+sudo apt-get install default-jdk
+sudo apt-get install nvidia-cuda-dev
+sudo apt-get install nvidia-cuda-toolkit
 
