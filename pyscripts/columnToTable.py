58dae308696c295f74a730fe43af2127a7acb8ae
Derrick Zwickl <zwickl@DizzyLion.local>
2013-11-01 12:24:33 -0700

    cleanup and enhancement

--- pyscripts/columnToTable.py
+++ pyscripts/columnToTable.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
 import sys
-import doctest
 
 from argparse import ArgumentParser
 
-#args are the name of the option, the actual flag that was used, the value, and the parser itself
-#Note that I'm not sure if assigning the split back to value is a good practice, but it works
-def comma_split(option, opt_str, value, parser):
-    intList = []
-    strList = value.split(",")
-    for s in strList:
-        intList.append(int(s))
-        setattr(parser.values, option.dest, intList)
-
 def print_usage(errMess):
-    if len(errMess) != 0:
+    if errMess:
         sys.stderr.write(errMess)
     parser.print_help()
     sys.stderr.write("i.e., given this inp\n")
@@ -29,24 +19,10 @@ def print_usage(errMess):
     sys.stderr.write("1    a    poo    roo\n")
     sys.stderr.write("2    b    moo    goo\n")
     sys.stderr.write("3    c    doo    noo\n")
-    exit()
+    sys.exit()
 
-def StringToIntOrFloat(string):
-    """name says it all
-    #>>> StringToIntOrFloat("1.2")
-    #1.2
-    """
-    try:
-        ret = int(string)
-    except ValueError:
-        ret = float(string)
-    except:
-        sys.stderr.write("string %s could not be interpreted as an int or float\n" % string)
-        sys.exit(1)
-    return ret
 
 class row(object):
-    
     def __init__(self, rowStr):
         #this saves the entries as strings
         self.colEntries = rowStr.strip().split()
@@ -63,25 +39,23 @@ class row(object):
 
     def __print__(self):
         print '\t'.join(self.colEntries)
-        #for ce in self.colEntries:
-        #    print ce,
+
 
 class rowset(object):
-    #rows = []
     def __init__(self):
         self.rows = []
 
     def append_row(self, rStr):
         self.rows.append(row(rStr))
 
-    def column_to_list(self, cnum, missingString=None):
+    def column_to_list(self, cnum, missing_string=None):
         clist = []
         for poo in self.rows:
             try:
                 clist.append(poo.element(cnum))
             except IndexError:
-                if missingString:
-                    clist.append(missingString)
+                if missing_string:
+                    clist.append(missing_string)
                 else:
                     raise
         #clist = [ poo.element(cnum) for poo in self.rows ]
@@ -96,64 +70,61 @@ class rowset(object):
     def __print__(self):
         for r in self.rows:
             r.__print__()
-            #print
 
-#this just runs the tests embedded in doc strings
-#if __name__ == "__main__":
-#    doctest.testmod()
+    def __len__(self):
+        return len(self.rows)
 
 parser = ArgumentParser()
 
-parser.add_argument("-i", "--input-file", dest="filename", default=None, type=str, help="input file")
-parser.add_argument("-m", "--missing-string", dest="missingString", default=None, type=str, 
-        help="allow missing values, and replace with specified string")
+parser.add_argument("-i", "--input", default=sys.stdin, type=FileType('rU'), help="input file")
+parser.add_argument("-m", "--missing-string", default=None, type=str, 
+                    help="allow missing values, and replace with specified string")
+
 parser.add_argument("-r", "--rows", dest="nrows", type=int, required=True, help="number of rows per chunk")
-parser.add_argument("-f", "--first-cols", dest="firstCols", nargs="*", type=int, default=None,
-        help="columns to use in first row set")
-parser.add_argument("-c", "--cols", dest="cols", nargs="*", type=int, default=None, help="columns to use in successive row sets")
-parser.add_argument("-t", "--transpose", action="store_true", dest="transflag", default=False, help="transpose matrix")
+
+parser.add_argument("-f", "--first-cols", nargs="*", type=int, default=None,
+                    help="columns to use in first row set")
+
+parser.add_argument("-c", "--cols", nargs="*", type=int, default=None, help="columns to use in successive row sets")
+
+parser.add_argument("-t", "--transpose", action="store_true", default=False, help="transpose matrix")
+parser.add_argument("-q", "--quiet", action="store_true", dest="quiet", default=False, help="no messages output to the screen")
+parser.add_argument("--skip-rows", type=int, default=0, help="initial rows to skip (NOTE: the --rows specification should not include these rows)")
 
 options = parser.parse_args()
 
-if options.filename is None:
-    sys.stderr.write("No filename passed (-i), assuming stdin\n")
-
-infile = open(options.filename, "ri") if options.filename else sys.stdin
-
 chunks = []
 rset = rowset()
-rnum = 0
-for line in infile:
-    rset.append_row(line)
-    rnum = rnum + 1
-    if rnum == options.nrows:
-        chunks.append(rset)
-        rset = rowset()
-        rnum = 0
+for num, line in enumerate(options.input):
+    if num >= options.skip_rows:
+        rset.append_row(line)
+        if len(rset) == options.nrows:
+            chunks.append(rset)
+            rset = rowset()
+
+if rset:
+    if not options.quiet:
+        sys.stderr.write('WARNING - last set of rows has fewer than --rows value.  Check --rows\n')
+    chunks.append(rset)
 
 #if specific columns weren't passed in, use them all.
 #remember that it assumes that they are specified starting at 1
-if options.firstCols is None:
-    firstCols = range(1, chunks[0].columns() + 1)
-else:
-    firstCols = options.firstCols
+firstCols = options.first_cols or range(1, chunks[0].columns() + 1)
+cols = options.cols or range(1, chunks[1].columns() + 1)
 
-if options.cols is None:
-    cols = range(1, chunks[1].columns() + 1)
-else:
-    cols = options.cols
-
-sys.stderr.write("Found %d total sets\n" % len(chunks))
-sys.stderr.write("using columns %s from the first set\n" % ", ".join([str(f) for f in firstCols]))
-sys.stderr.write("and columns %s from later sets\n" % ", ".join([str(f) for f in cols]))
+if not options.quiet:
+    sys.stderr.write("Found %d total sets\n" % len(chunks))
+    sys.stderr.write("using columns %s from the first set\n" % ", ".join([str(f) for f in firstCols]))
+    sys.stderr.write("and columns %s from later sets\n" % ", ".join([str(f) for f in cols]))
 
 #get the proper columns from the first set
-tableCols = [chunks[0].column_to_list(c - 1, options.missingString) for c in firstCols]
+tableCols = [chunks[0].column_to_list(c - 1, options.missing_string) for c in firstCols]
 
-for thisChunk in chunks[1:]:
-    tableCols.extend(thisChunk.column_to_list(c - 1, options.missingString) for c in cols)
+if len(chunks) > 1:
+    for thisChunk in chunks[1:]:
+        tableCols.extend(thisChunk.column_to_list(c - 1, options.missing_string) for c in cols)
 
-if not options.transflag:
+if not options.transpose:
     #nested list comp
     print "\n".join(["\t".join([col[r] for col in tableCols]) for r in xrange(options.nrows)])
     
--- pyscripts/sumCol.py
+++ pyscripts/sumCol.py
@@ -1,81 +1,67 @@
 #!/usr/bin/env python
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 
 #use argparse module to parse commandline input
 parser = ArgumentParser(description='sum or do other summaries of a column of numbers')
 
-parser.add_argument("-s", "--sum", action="store_true", dest="outputSum", default=False, help="Output Sum")
+parser.add_argument("-s", "--sum", action="store_true", default=False, help="Output Sum")
 
-parser.add_argument("--ignore-non-numeric", action="store_true", dest="ignoreNonNumeric", default=False, help="Ignore any column elements that can't be converted to floats")
+parser.add_argument("--ignore-non-numeric", action="store_true", default=False, help="Ignore any column elements that can't be converted to floats")
 
-parser.add_argument("-m", "--mean", action="store_true", dest="outputAve", default=False, help="Output Mean")
+parser.add_argument("-m", "--mean", action="store_true", default=False, help="Output Mean")
 
-parser.add_argument("-r", "--range", action="store_true", dest="outputMinMax", default=False, help="Output Min and Max")
+parser.add_argument("-r", "--range", action="store_true", default=False, help="Output Min and Max")
 
-parser.add_argument("-a", "--all",  action="store_true", dest="outputAll", default=False, help="Output All Statistics")
+parser.add_argument("-a", "--all",  action="store_true", default=False, help="Output All Statistics")
 
-parser.add_argument("-c", "--column", dest="columnNum", default=None, type=int, help="choose the column to output")
+parser.add_argument("-c", "--column", default=None, type=int, help="choose the column to output")
 
-#variable number of arguments
-parser.add_argument('filenames', nargs='*', default=[], 
-                   help='a list of filenames to search (none for stdin)')
+parser.add_argument("-p", "--precision", default=4, type=int, help="number of digits past decimal for floating point (default %(default)")
+
+parser.add_argument('infile', nargs='?', default=sys.stdin, type=FileType('rU'), help='filename to search (none for stdin)')
 
 #now process the command line
 options = parser.parse_args()
 
-if options.outputAll:
-    oSum = True
-    oAve = True
-    oMinMax = True
+if options.all:
+    oSum, oAve, oMinMax = True, True, True
 else:
-    oSum = options.outputSum
-    oAve = options.outputAve
-    oMinMax = options.outputMinMax
-
+    oAve = options.mean
+    oMinMax = options.range
     #if no options were entered, assume sum, otherwise -s is required to output the sum
-    if not oAve and not oMinMax:
-        oSum = True
+    oSum = True if not (oAve or oMinMax) else options.sum
 
-if len(options.filenames) == 1:
-    filename = options.filenames[0]
-    try:
-        infile = open(filename, 'rb')
-    except IOError:
-        raise RuntimeError("problem opening file")
-elif options.filenames:
-    sys.exit("expecting only one filename, got %s" % len(options.filenames))
-else:
-    infile = sys.stdin
+lines = [ line.strip().split() for line in options.infile ]
 
-lines = [ line.strip().split() for line in infile ]
-
-if options.columnNum is None:
+if options.column is None:
     if lines and len(lines[0]) > 1:
         raise RuntimeError('must pass either a single column or use the --column flag')
     else:
-        options.columnNum = 1
+        options.column = 1
 
-colIndex = options.columnNum - 1
+colIndex = options.column - 1
 col = []
 for line in lines:
     try:
         val = float(line[colIndex])
     except ValueError:
-        if options.ignoreNonNumeric:
+        if options.ignore_non_numeric:
             sys.stderr.write('ignoring element \'%s\'\n' % line[colIndex])
             val = None
         else:
             raise
     except IndexError:
-        exit('could not find column %d of line\n%s' % (options.colNum, line))
+        sys.exit('could not find column %d of line\n%s' % (options.column, line))
     #need to allow val to be zero here
     if val not in [ None, '' ]:
         col.append(val)
 
+'''no lines read shouldn't really be a problem
 if not col:
     sys.stderr.write('No valid values read!\n')
     exit()
+'''
 
 cSum = sum(col)
 cNum = len(col)
@@ -85,7 +71,8 @@ out = []
 if oSum:
     out.append(str(cSum))
 if oAve:
-    out.append(str(cSum / cNum))
+    precString = '%%.%df' % options.precision
+    out.append(precString % (cSum / cNum))
 if oMinMax:
     out.append(str(min(col)))
     out.append(str(max(col)))
