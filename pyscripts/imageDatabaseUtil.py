8444c9cc5197f4758310f5e109aba1f88bfa442f
Laura Zwickl <LTZ@Users-MacBook.local>
2014-09-01 17:08:40 -0700

    improvements to image util

--- pyscripts/imageDatabaseUtil.py
+++ pyscripts/imageDatabaseUtil.py
@@ -16,7 +16,8 @@ def md5Checksum(filePath):
     with open(filePath, 'rb') as fh:
         m = md5()
         while True:
-            data = fh.read(8192)
+            #data = fh.read(8192)
+            data = fh.read(16384)
             if not data:
                 break
             m.update(data)
@@ -75,7 +76,11 @@ class ImageFile(FileInfo):
     def tuple_repr(self):
         ex_time = self.exif_time or 'None'
         #print self.exif_time
-        return (self.path, ex_time, self.file_time, self.dir, self.md5)
+        return (self.path, self.dir, self.fname, self.file_time, ex_time, self.md5)
+
+    def header(self):
+        return 'path\texif_time\tfile_time\tdir'
+
 
 def open_image_shelf(dbfile):
     if os.path.exists(dbfile):
@@ -105,6 +110,27 @@ def open_image_shelf(dbfile):
     return (img_shelf, img_dict, known_paths)
 
 
+def make_tabular_string(img_dict):
+    #print '#' + ' '.join(d1[key].ljust(33) for key in d1.keys())
+    print '#' + ' '.join(img_dict[key].ljust(width) for key, width in zip(img_dict.keys(), [75, 16, 16, 16, 30]))
+
+
+def make_tabular_header(img_dict):
+    #print '#' + ' '.join(d1[key].ljust(33) for key in d1.keys())
+    print '#' + ' '.join(key.ljust(width) for key, width in zip(img_dict.keys(), [75, 16, 16, 16, 30]))
+
+
+def element_identical(first, sec, el):
+    return first[el] == sec[el]
+
+
+def identical_elements_string(first, sec):
+    ident = []
+    for el in ['path', 'dir', 'filename', 'file_time', 'exif_time', 'md5']:
+        if element_identical(first, sec, el):
+            ident.append(el)
+    return ' '.join(ident)
+
 
 parser = argparse.ArgumentParser(description='Create a database and manipulate image files')
 
@@ -122,11 +148,15 @@ parser.add_argument('--read-only', action='store_true', default=False, help='per
 
 parser.add_argument('--minimal', action='store_true', default=False, help='only output a list of filenames for the chosen option, no other information')
 
-#parser.add_argument('-db', '--database', default='database.sql', help='name of database file to load/create (default database.shelf)')
-parser.add_argument('-db', '--database', default='images', help='name of database file to load/create (default database.shelf)')
+parser.add_argument('-db', '--database', default='database.sql', help='name of database file to load/create (default database.sql)')
+#parser.add_argument('-db', '--database', default='images', help='name of database file to load/create (default images)')
 
 #parser.add_argument('-db2', '--database2', default='database2.sql', help='name of 2nd database file to load for set comparisons (default database2.shelf)')
-parser.add_argument('-db2', '--database2', default='images2', help='name of 2nd database file to load for set comparisons (default database2.shelf)')
+#parser.add_argument('-db2', '--database2', default='images2', help='name of 2nd database file to load for set comparisons (default images2)')
+
+parser.add_argument('-t', '--table', default='table1', help='name of table in database to load/create (default is "table1")')
+parser.add_argument('-t2', '--table2', default=None, help='name of 2nd table in database use in table comparisons with --action')
+
 
 parser.add_argument('--action', default=None, choices=['intersection', 'union', 'xor', 'minus', 'rminus'], help='set operations to perform on two databases')
 
@@ -141,11 +171,11 @@ use_writeback = False
 if not options.action:
     #img_shelf, img_dict, known_paths = open_image_shelf(options.database)
 
-    conn = sqlite3.connect('database.sql')
+    conn = sqlite3.connect(options.database)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
     c.execute('''CREATE TABLE if not exists %s
-            (path TEXT PRIMARY KEY, exif_time TEXT, file_time TEXT, dir TEXT, md5 TEXT)''' % options.database)
+            (path TEXT PRIMARY KEY, dir TEXT, filename TEXT, file_time TEXT, exif_time TEXT, md5 TEXT)''' % options.table)
 
     img_dict = {}
     
@@ -154,7 +184,7 @@ if not options.action:
     for fn in options.files:
         if os.path.isfile(fn):
                 
-            c.execute('SELECT * FROM %s WHERE path=?' % options.database, (fn,))
+            c.execute('SELECT * FROM %s WHERE path=?' % options.table, (fn,))
             if not c.fetchone():
                 if numAdded == 0:
                     sys.stderr.write('adding files ...\n')
@@ -169,52 +199,95 @@ if not options.action:
         else:
             sys.stderr.write('%s is not a file. Skipping.' % fn)
 
-    sys.stderr.write('\n%d items added to database\n%d items were already in database\n%d total items in database\n' % (numAdded, numHad, len(img_dict)))
-    c.executemany("INSERT OR IGNORE INTO %s VALUES (?, ?, ?, ?, ?)" % options.database, [ img.tuple_repr() for img in flatten_array(img_dict.values()) ])
+    c.execute('SELECT COUNT(*) AS num FROM %s' % options.table)
+    total = c.fetchone()[0]
+
+    #sys.stderr.write('\n%d items added to database\n%d items were already in database\n%d total items in database\n' % (numAdded, numHad, len(img_dict)))
+    sys.stderr.write('\n%d items added to database\n%d items were already in database\n%d total items in database\n' % (numAdded, numHad, total))
+    c.executemany("INSERT OR IGNORE INTO %s VALUES (?, ?, ?, ?, ?, ?)" % options.table, [ img.tuple_repr() for img in flatten_array(img_dict.values()) ])
  
     if options.duplicates:
-        c.execute("SELECT md5 FROM %s GROUP BY md5 HAVING count(md5) > 1" % options.database)
+        c.execute("SELECT md5 FROM %s GROUP BY md5 HAVING count(md5) > 1" % options.table)
+        first = True
         for md5 in c.fetchall():
             #for row in c.execute("SELECT * FROM %s WHERE md5=?" % options.database, (md5[0],)):
             #    print row
             #print
-            dupes = [ row for row in c.execute("SELECT * FROM %s WHERE md5=?" % options.database, (md5[0],)) ]
+            dupes = [ row for row in c.execute("SELECT * FROM %s WHERE md5=?" % options.table, (md5[0],)) ]
             #print '\n'.join([str(d) for d in dupes])
             for n1, d1 in enumerate(dupes):
                 for d2 in dupes[n1 + 1:]:
+                    if first:
+                        make_tabular_header(d1)
+                        first = False
                     if list(d1)[1:] == list(d2)[1:]:
-                        print 'exact dupe'
-                        print '#' + ' '.join(d1[key].ljust(33) for key in d1.keys())
-                        print '#' + ' '.join(d2[key].ljust(33) for key in d2.keys())
-                        print '#rm %s' % d1['path']
-                        print '#rm %s' % d2['path']
-                    else:
-                        print '#' + ' '.join(d1[key].ljust(33) for key in d1.keys())
-                        print '#' + ' '.join(d2[key].ljust(33) for key in d2.keys())
-                        print '#rm %s' % d1['path']
-                        print '#rm %s' % d2['path']
+                        print '#all but filename dupe'
+                    elif list(d1)[2:] == list(d2)[2:]:
+                        print '#all but directory dupe'
+                    elif list(d1)[3:] == list(d2)[3:]:
+                        print '#exif-md5 dupe'
+                    sys.stdout.write('#%s\n' % identical_elements_string(d1, d2))
+                    make_tabular_string(d1)
+                    make_tabular_string(d2)
 
+                    #first favor pics where the exif/dir match
+                    remove_first = None
+                    if d1['exif_time'] not in [None, 'None'] and d1['exif_time'] != d1['dir']:
+                        remove_first = True
+                    elif d2['exif_time'] not in [None, 'None'] and d2['exif_time'] != d2['dir']:
+                        remove_first = False
+                    else:
+                        len1 = len(d1['path'])
+                        len2 = len(d2['path'])
+
+                        #then favor the shorter path
+                        if len1 != len2:
+                            if len1 < len2:
+                                remove_first = False
+                            else:
+                                remove_first = True
+                        #finally, favor the alphanumerically first
+                        elif d1['path'] < d2['path']:
+                            remove_first = False
+                        else:
+                            remove_first = True
+
+                    if remove_first:
+                        print 'rm "%s"' % d1['path']
+                        print '#rm "%s"' % d2['path']
+                    else:
+                        print '#rm "%s"' % d1['path']
+                        print 'rm "%s"' % d2['path']
                     print
 
+    first = True
     if options.no_exif:
-        for row in c.execute("SELECT * FROM %s WHERE exif_time=? ORDER BY md5" % options.database, ('None',)):
-            print '#' + ' '.join(row[key].ljust(33) for key in row.keys())
-            #print row
+        for row in c.execute("SELECT * FROM %s WHERE exif_time=? ORDER BY md5" % options.table, ('None',)):
+            if first:
+                make_tabular_header(row)
+                first = False
+            make_tabular_string(row)
     
     if options.exif_file_mismatch:
-        for row in c.execute("SELECT * FROM %s WHERE not exif_time=file_time AND not exif_time=?" % options.database, ('None',)):
-            print '#' + ' '.join(row[key].ljust(33) for key in row.keys())
-            #print row
+        for row in c.execute("SELECT * FROM %s WHERE not exif_time=file_time AND not exif_time=?" % options.table, ('None',)):
+            if first:
+                make_tabular_header(row)
+                first = False
+            make_tabular_string(row)
 
     if options.exif_dir_mismatch:
-        for row in c.execute("SELECT * FROM %s WHERE not exif_time=dir AND not exif_time=?" % options.database, ('None',)):
-            print '#' + ' '.join(row[key].ljust(33) for key in row.keys())
-            #print row
+        for row in c.execute("SELECT * FROM %s WHERE not exif_time=dir AND not exif_time=?" % options.table, ('None',)):
+            if first:
+                make_tabular_header(row)
+                first = False
+            make_tabular_string(row)
 
     if options.list:
-        for row in c.execute("SELECT * FROM %s WHERE path GLOB ?" % options.database, ('*%s*' % options.list,)):
-            print '#' + ' '.join(row[key].ljust(33) for key in row.keys())
-    
+        for row in c.execute("SELECT * FROM %s WHERE path GLOB ?" % options.table, ('*%s*' % options.list,)):
+            if first:
+                make_tabular_header(row)
+                first = False
+            make_tabular_string(row)
 
     # Save (commit) the changes
     conn.commit()
@@ -222,34 +295,32 @@ if not options.action:
     sys.exit()
 
 else:
-
-    conn = sqlite3.connect('database.sql')
+    conn = sqlite3.connect(options.database)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-
     
     if options.action in ['minus', 'rminus', 'xor']:
         if options.action  == 'minus':
-            one, two = options.database, options.database2
+            one, two = options.table, options.table2
         else:
-            two, one = options.database, options.database2
+            two, one = options.table, options.table2
         c.execute("SELECT md5 FROM %s EXCEPT SELECT md5 from %s" % (one, two))
         res = c.fetchall()
         if not options.action == 'xor':
             sys.stderr.write('subtraction has %d entries\n' % len(res))
             for md5 in res:
                 for row in c.execute("SELECT * FROM %s WHERE md5=?" % one, (md5[0],)):
-                    print '#' + ' '.join(row[key].ljust(33) for key in row.keys())
+                    make_tabular_string(row)
         else:
             c.execute("SELECT md5 FROM %s EXCEPT SELECT md5 from %s" % (two, one))
             res.extend(c.fetchall())
             sys.stderr.write('xor has %d entries\n' % len(res))
             for md5 in res:
                 for row in c.execute("SELECT * FROM %s WHERE md5=?" % one, (md5[0],)):
-                    print '#' + ' '.join(row[key].ljust(33) for key in row.keys())
+                    make_tabular_string(row)
             
     elif options.action == 'intersection':
-        one, two = options.database, options.database2
+        one, two = options.table, options.table2
         c.execute("SELECT md5 FROM %s EXCEPT SELECT md5 from %s" % (one, two))
 
 
